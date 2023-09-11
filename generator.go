78e0f693ae8f0ae053dfe8cf40b3b4b732526c6c
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-09-11 14:09:29 +0100

    Finished GenerateFunctionSymbol

--- generator.go
+++ generator.go
@@ -9,6 +9,27 @@ func GenerateRegisterFunction(packageName string, function *ast.FuncDecl) string
 	return fmt.Sprintf("registerFunction(\"%s\", %s)", packageName, GenerateFunctionSymbol(packageName, function))
 }
 
+func GenerateFunctionSymbol(packageName string, function *ast.FuncDecl) string {
+	// ReRect only currently allows for a single return type so anything returning multiple values will probably be ignored for now.
+
+	returnType := ""
+	if function.Type.Results.NumFields() == 0 {
+		returnType = "void"
+	} else {
+		returnType = "" //function.Type.Results.List[0].Type
+	}
+
+	return fmt.Sprintf(
+		"symbols.NewVMFunctionSymbol(%s, \"%s\", %s, %s, %s)",
+		packageName,
+		function.Name.String(),
+		GenerateGlobalDataTypeRegister(returnType),
+		GenerateParameterSymbolSlice(function),
+		function.Name.String(),
+	)
+}
+
+
 func GenerateRegisterPackage(packageName string) string {
 	return fmt.Sprintf("%s := registerPackage(\"%s\")\n", packageName, packageName)
 }
