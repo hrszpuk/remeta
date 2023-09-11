bbf08c00b187dfa33968fd5b8ce69bf83c05183b
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-09-11 14:10:25 +0100

    Fixed grabber data disappearance issues
    I'm dumb, i forgot about pointers.

--- grabber.go
+++ grabber.go
@@ -1,34 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"go/ast"
 )
 
-type FuncData struct {
-	Name string
-	Type ast.FuncType
-}
-
 type Grabber struct {
-	Functions []FuncData
+	Functions []ast.FuncDecl
 }
 
-func (g Grabber) GrabAll(file *ast.File) {
+func (g *Grabber) GrabAll(file *ast.File) {
+	g.Functions = make([]ast.FuncDecl, 0)
 	ast.Walk(g, file)
 }
 
-func (g Grabber) Visit(n ast.Node) ast.Visitor {
+func (g *Grabber) Visit(n ast.Node) ast.Visitor {
 	if n == nil {
 		return nil
 	}
 
 	switch d := n.(type) {
 	case *ast.FuncDecl:
-		fmt.Printf("%v\n", d)
-		f := FuncData{d.Name.Name, *d.Type}
-		g.Functions = append(g.Functions, f)
-		fmt.Println(g.Functions)
+		g.Functions = append(g.Functions, *d)
 	}
 
 	return g
