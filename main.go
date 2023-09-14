fc403817ceed6361e3247666a7ba83744d6a3a47
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-09-14 14:49:20 +0100

    Added logging

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"go/parser"
 	"go/token"
+	"log/slog"
+	"os"
+	"strings"
 )
 
+var loggerLevel = new(slog.LevelVar)
+var logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: loggerLevel}))
+
 func main() {
+	debugPtr := flag.Bool("debug", false, "for printing debug logs")
+	flag.Parse()
+
+	if *debugPtr {
+		loggerLevel.Set(slog.LevelDebug)
+	} else {
+		loggerLevel.Set(slog.LevelError)
+	}
+
+	if len(flag.Args()) < 2 {
+		fmt.Println("Usage: remeta <path/to/package> <output-name>")
+		return
+	}
+
 	fset := token.NewFileSet()
 
 	f, err := parser.ParseFile(fset, "test/test.go", nil, parser.AllErrors)
